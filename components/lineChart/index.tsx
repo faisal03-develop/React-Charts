fd481ff263de63af5bfe9acfef5a235f2f7d7193
Faisal Altaf <altaffaisal532@gmail.com>
2026-01-29 11:38:56 +0500

    Added the Folder Structure and installed the CHarts Dependencies

---
+++ components/lineChart/index.tsx
@@ -0,0 +1,25 @@
+
+export default function LineChart() {
+  return (
+    <div>
+        LineChart
+        <div>
+            option = {
+                xAxis: {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                    data: [150, 230, 224, 218, 135, 147, 260],
+                    type: 'line'
+                    }
+                ]
+                };
+        </div>
+    </div>
+  )
+}
