e700f7eadfe2b797b9e5a42c4c63621d72ee4f7b
Faisal Altaf <altaffaisal532@gmail.com>
2026-01-29 14:50:03 +0500

    Name Corrections

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import LineChart from "@/components/lineChart/index";
 import BarChart from "@/components/barChart/index";
 import AreaChart from "@/components/areaStyle/index";
 import { ChartData } from "@/data/chartData";
-
+import StackedLine from '@/components/stackedLine/index'
 export default function Home() {
   return (
     <div>
       <h1 className="text-5xl font-bold text-center px-auto my-10 mb-30">Charts</h1>
-      <div className="grid grid-cols-2 gap-10 place-items-center">
+      <div className="grid grid-cols-2 gap-10 place-items-center pb-20">
         <div className="flex flex-col items-center border-2 border-gray-400 w-150">
           <LineChart numbers={ChartData} />
           <span className="text-2xl font-medium">LineChart</span>
@@ -18,11 +18,11 @@ export default function Home() {
         </div>
         <div className="flex flex-col items-center border-2 border-gray-400 w-150">
           <AreaChart numbers={ChartData} />
-          <span className="text-2xl font-medium">BarChart</span>
+          <span className="text-2xl font-medium">AreaStyle</span>
         </div>
         <div className="flex flex-col items-center border-2 border-gray-400 w-150">
-          <BarChart numbers={ChartData} />
-          <span className="text-2xl font-medium">BarChart</span>
+          <StackedLine />
+          <span className="text-2xl font-medium">StackedLine</span>
         </div>
       </div>
     </div>
